9d34e32dc0142ddf3376694bfc0632f8e6965de6
severmanolescu <severmanolescu1@gmail.com>
2023-11-04 14:00:15 +0200

    Create empty project

---
+++ Packy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Packy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
